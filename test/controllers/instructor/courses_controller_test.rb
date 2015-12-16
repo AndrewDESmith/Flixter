32d02aa0fc5dd2504d23c3af0d8f888f1c6bc0ba
AndrewDESmith <andrew.d.e.smith@gmail.com>
2015-12-16 21:42:29 +0000

    Add validations to course creation (title and description cannot be blank, cost >= 0)

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -16,21 +16,23 @@ class Instructor::CoursesControllerTest < ActionController::TestCase
   test "a user can create a new course" do
     user = FactoryGirl.create(:user)
     sign_in user
-    # course = FactoryGirl.create(:course)
-    # visit new_instructor_course_path
-    # fill_in "Title", :with => course.title
-    # fill_in "Description", :with => course.description
-    # fill_in "Cost", :with => course.cost
-    # click_button "Create"
-    post :create,
-      :course => {
-        :title => 'Blah',
-        :description => 'How to blah',
-        :cost => 0.00
-      }
+    # .attributes_for gives the attributes without being connected to a database model
+    # .slice takes a hash
+    post :create, :course => FactoryGirl.attributes_for(:course).slice(:title, :description, :cost)
     assert_equal 1, user.courses.count
     # Fetch the last course from the database AFTER submitting the form, this will be the newly entered course, which will be the page it will redirect to.
     course = Course.last
     assert_redirected_to instructor_course_path(course.id)
+  end
+
+  test "refuse invalid data on course creation" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    post :create, :course => {
+      :title => '',
+      :description => '',
+      :cost => -1
+    }
+    assert_response :unprocessable_entity
   end
 end
