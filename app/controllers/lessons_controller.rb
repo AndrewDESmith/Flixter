52c992f415b41f542d6786a8db240080a6393b13
AndrewDESmith <andrew.d.e.smith@gmail.com>
2016-01-04 21:32:40 +0000

    Ensure only enrolled users can access a lesson show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, :only => [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    course = current_lesson.section.course
+    if !current_lesson.section.course.user.enrolled_in?(course)
+      redirect_to course_path(course), :alert => 'You need to enroll in this course to view a lesson'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class LessonsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "only enrolled users can view the lesson show page" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    course = FactoryGirl.create(:course, :user => user)
+    section = FactoryGirl.create(:section, :course => course)
+    lesson = FactoryGirl.create(:lesson, :section => section)
+    get :show, :id => lesson.id
+    assert_redirected_to course_path(course)
+  end
 end
