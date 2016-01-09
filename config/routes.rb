0bedbdd00ada9d3f7ba3defe7de4e45077986b52
AndrewDESmith <andrew.d.e.smith@gmail.com>
2016-01-09 20:00:31 +0000

    Enable instructors to reorder lessons in their own courses, using jQuery UI sortable

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Flixter::Application.routes.draw do
   resources :lessons, :only => [:show]
   namespace :instructor do
     # Set up a sections route without any actions solely to nest lessons underneath it, to avoid lessons being underneath a course id in the URL.  This is so that a user cannot manually go to the new lesson form for the wrong course.
+    resources :lessons, :only => [:update]
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
