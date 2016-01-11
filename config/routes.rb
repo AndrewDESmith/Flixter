4b6c1a5c3dad43d9ea25af76a4884c65cec7c4f7
AndrewDESmith <andrew.d.e.smith@gmail.com>
2016-01-11 18:02:38 +0000

    Add a dashboard listing courses the user is enrolled in, and also listing courses the user is teaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Flixter::Application.routes.draw do
   devise_for :users
-  # devise_for :users
+  # Singleton route (no id passed):
+  resource :dashboard, :only => [:show]
   root 'static_pages#index'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
@@ -9,8 +10,7 @@ Flixter::Application.routes.draw do
   namespace :instructor do
     # Set up a sections route without any actions solely to nest lessons underneath it, to avoid lessons being underneath a course id in the URL.  This is so that a user cannot manually go to the new lesson form for the wrong course.
     resources :lessons, :only => [:update]
-    resources :sections, :only => [:update]
-    resources :sections, :only => [] do
+    resources :sections, :only => [:update] do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
